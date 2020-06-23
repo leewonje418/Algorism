c03162e2583140fccac9feb37c7457e0d2069e84
leewonje418 <kitti0418@naver.com>
2020-06-23 17:26:07 +0900

    2020.06.23(1)

---
+++ src/Q3/Linked_List/Linkedlist.java
@@ -0,0 +1,30 @@
+package Q3.Linked_List;
+
+public class Linkedlist {
+	public static void main(String[] args) {
+		
+		Problem list = new Problem();
+		
+		list.add(100);
+		list.add(100);
+		list.add(400);
+		list.add(500);
+		System.out.println(list);
+		
+		list.add(2, 300);
+		list.addFirst(50);
+		System.out.println(list);
+		
+		System.out.println(list.get(4));
+		
+		list.remove(2);
+		list.remove(new Integer(400));
+		System.out.println(list);
+		
+		list.removeFirst();
+		list.removeLast();
+		System.out.println(list);
+		
+		System.out.println("Å©±â : " + list.size());
+	}
+}
